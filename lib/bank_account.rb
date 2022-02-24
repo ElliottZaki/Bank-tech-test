6faa25d506b09bed39cd860f1e916ff5b21a4c57
ElliottZaki <spudnick99@hotmail.com>
2022-02-24 14:07:28 +0000

    Changed balance method to statement, updated depoist and withdraw methods
    Changed the balance method to 'statement', which creates an empty array that details can now be pushed too
    Such as a date of transaction, deposit and withdrawl amount

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,20 +1,28 @@
 class Bankaccount
 
-  attr_accessor :balance
+  attr_accessor :balance, :statement
 
-  def initialize(balance)
-    @balance = balance
+  def initialize(initial_deposit)
+    @balance = initial_deposit
+    @deposit_amount = 0
+    @withdrawal_amount = 0
+    @date = ''
+    @statement = []
   end
 
-  def balance
-    return @balance
+  def statement
+    return @statement
   end
 
-  def deposit(amount)
-    @balance += amount
+  def deposit(amount, date)
+    @statement.push(@deposit_amount = amount)
+    @statement.push(@date = date)
+    @statement.push(@balance += amount)
   end
 
-  def withdraw(amount)
-    @balance -= amount
+  def withdraw(amount, date)
+    @statement.push(@withdrawal_amount = amount)
+    @statement.push(@date = date)
+    @statement.push(@balance -= amount)
   end
 end 
--- spec/features/bank_account_spec.rb
+++ spec/features/bank_account_spec.rb
@@ -1,25 +1,26 @@
 require 'bank_account'
 
 describe Bankaccount do
-  it 'creates a new bank account object, with a balance of £0' do
+  it 'creates a new bank account object, with a blank statement array' do
     account = Bankaccount.new(0)
 
-    expect(account.balance).to eq(0)
+    expect(account.statement).to eq([])
   end
 
   it 'deposits money into the bank account' do
     account = Bankaccount.new(0)
-    account.deposit(100)
+    account.deposit(100, '14/02/2022')
+    account.deposit(50, "15/02/2022")
 
-    expect(account.balance).to eq(100)
+    expect(account.statement).to eq([100, "14/02/2022", 100, 50, "15/02/2022", 150])
   end
   
   it 'withdraws money from the bank account' do
     account = Bankaccount.new(0)
-    account.deposit(100)
-    account.withdraw(50)
+    account.deposit(100, '14/02/2022')
+    account.withdraw(50, '15/02/2022')
 
-    expect(account.balance).to eq(50)
+    expect(account.statement).to eq([100, "14/02/2022", 100, 50, "15/02/2022", 50])
   end 
 end
 
